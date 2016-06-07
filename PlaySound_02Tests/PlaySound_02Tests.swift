3d370c166f1fdec0c29c3d37b71cf79d642d1b7e
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-06-07 22:31:57 +0800

    Initial Commit

---
+++ PlaySound_02Tests/PlaySound_02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PlaySound_02Tests.swift
+//  PlaySound_02Tests
+//
+//  Created by MikeWu on 2016/6/7.
+//  Copyright © 2016年 PlaySound. All rights reserved.
+//
+
+import XCTest
+@testable import PlaySound_02
+
+class PlaySound_02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
